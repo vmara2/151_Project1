579e5f84fb78dc5d185e0773f7a6c5c051988ddf
vmara2 <32969858+vmara2@users.noreply.github.com>
2020-08-26 14:21:41 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,13 @@
 var x = 0;
 var speed = 3;
 
+var ball = {
+  x: 150,
+  y: 150,
+  xspeed: 4,
+  yspeed: -3
+}
+
 function setup() {
   createCanvas(400,400);
   // put setup code here
@@ -8,16 +15,26 @@ function setup() {
 
 function draw() {
   background(100,100,100);
-  rect(x,100,110,110);
-  fill(20,30,100);
-
-  if (x > width - 100) {
-    speed = -speed;
-  }
-  else if (x <= 3) {
-    speed = 3;
-  }
-
-  x = x + speed;
+  move();
+  bounce();
+  display();
   // put drawing code here
+}
+function bounce() {
+  if (ball.x > width || ball.x < 0) {
+    ball.xspeed = ball.xspeed * -1;
+  }
+
+  if (ball.y > height || ball.y < 0) {
+    ball.yspeed = ball.yspeed * -1;
+  }
+}
+function display() {
+  fill(255, 0, 0);
+  rect(ball.x, ball.y, 25, 25);
+}
+
+function move() {
+  ball.x = ball.x + ball.xspeed;
+  ball.y = ball.y + ball.yspeed;
 }
