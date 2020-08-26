88e955c61ce2caa2caef4b03b8111d465b524f36
vmara2 <32969858+vmara2@users.noreply.github.com>
2020-08-26 13:21:03 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var x = 0;
+var speed = 3;
+
+function setup() {
+  createCanvas(400,400);
+  // put setup code here
+}
+
+function draw() {
+  background(100,100,100);
+  rect(x,100,110,110);
+  fill(20,30,100);
+
+  if (x > width - 100) {
+    speed = -speed;
+  }
+
+  x = x + speed;
+  // put drawing code here
+}
