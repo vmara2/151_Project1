a342635e010c6f8640eb9099eeef1548feebd5f7
vmara2 <32969858+vmara2@users.noreply.github.com>
2020-08-31 14:35:00 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,40 +1,34 @@
-var x = 0;
-var speed = 3;
-
-var ball = {
-  x: 150,
-  y: 150,
-  xspeed: 4,
-  yspeed: -3
+var cfill = {
+  r: 4,
+  g: 29,
+  b: 64
 }
 
 function setup() {
-  createCanvas(400,400);
+  createCanvas(windowWidth,windowHeight);
   // put setup code here
 }
 
 function draw() {
-  background(100,100,100);
-  move();
-  bounce();
-  display();
+  background(cfill.r, cfill.g, cfill.b);
+  circle(mouseX, mouseY, 50);
+  stroke(51);
+  strokeWeight(4);
+  fill(mouseX, mouseY, mouseY);
   // put drawing code here
 }
-function bounce() {
-  if (ball.x > width || ball.x < 0) {
-    ball.xspeed = ball.xspeed * -1;
-  }
 
-  if (ball.y > height || ball.y < 0) {
-    ball.yspeed = ball.yspeed * -1;
-  }
-}
-function display() {
-  fill(255, 0, 0);
-  rect(ball.x, ball.y, 25, 25);
+function mouseClicked() {
+  cfill.r = cfill.r + 5;
+  cfill.g = cfill.g + 5;
+  cfill.b = cfill.b + 5;
+  print('r: ' + cfill.r);
 }
 
-function move() {
-  ball.x = ball.x + ball.xspeed;
-  ball.y = ball.y + ball.yspeed;
+function keyPressed() {
+  if (keyCode == BACKSPACE) {
+    cfill.r = 4;
+    cfill.g = 29;
+    cfill.b = 64;
+  }
 }
